4ddb3c832af30e2141b9e505813cf9830771f3f5
GillisC <timcarlsson0203@gmail.com>
2022-05-01 20:29:07 +0200

    + IMatDataHandlerModel

---
+++ src/imat/IMatDataHandlerModel.java
@@ -0,0 +1,93 @@
+package imat;
+
+
+import se.chalmers.cse.dat216.project.*;
+
+import java.util.List;
+
+public class IMatDataHandlerModel {
+    private static IMatDataHandlerModel instance = null;
+    private IMatDataHandler iMatDataHandler;
+
+    /**
+     * Does nothing
+     */
+    protected IMatDataHandlerModel() {
+    }
+
+    /**
+     * Returns the singleton of IMatDataHandlerModel
+     * @return IMatDataHandlerModel
+     */
+    public static IMatDataHandlerModel getInstance() {
+        if (instance == null) {
+            instance = new IMatDataHandlerModel();
+            instance.init();
+        }
+        return instance;
+    }
+
+    private void init() {
+        iMatDataHandler = IMatDataHandler.getInstance();
+    }
+
+    /**
+     * Saves stored data to text files in imat folder
+     */
+    public void shutdown() {
+        iMatDataHandler.shutDown();
+    }
+
+    public Customer getCustomer() {
+        return instance.getCustomer();
+    }
+
+    public User getUser() {
+        return iMatDataHandler.getUser();
+    }
+
+    public CreditCard getCreditCard() {
+        return iMatDataHandler.getCreditCard();
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return iMatDataHandler.getShoppingCart();
+    }
+
+    public Order placeOrder() {
+        return iMatDataHandler.placeOrder();
+    }
+
+    public Order placeOrder(boolean clearShoppingCart) {
+        return iMatDataHandler.placeOrder(clearShoppingCart);
+    }
+
+    public List<Order> getOrders() {
+        return iMatDataHandler.getOrders();
+    }
+    public Product getProduct(int idNbr) {
+        return iMatDataHandler.getProduct(idNbr);
+    }
+    public List<Product> getProducts() {
+        return iMatDataHandler.getProducts();
+    }
+    public List<Product> getProducts(ProductCategory pc) {
+        return iMatDataHandler.getProducts(pc);
+    }
+    public List<Product> findProducts(String s) {
+        return iMatDataHandler.findProducts(s);
+    }
+    public void addProduct(Product p) {
+        iMatDataHandler.addProduct(p);
+    }
+    public void removeProduct(Product p) {
+        iMatDataHandler.removeProduct(p);
+    }
+    public void addFavorite(Product p) {
+        iMatDataHandler.addFavorite(p);
+    }
+    public void removeFavorite(Product p) {
+        iMatDataHandler.removeFavorite(p);
+    }
+
+}
