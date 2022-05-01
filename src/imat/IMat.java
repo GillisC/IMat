3ce14a71af7ffc0182c5411113425d9e599eb214
GillisC <timcarlsson0203@gmail.com>
2022-05-01 19:24:22 +0200

    fungerande skal

--- src/imat/IMat.java
+++ src/imat/IMat.java
@@ -1,4 +1,33 @@
 package imat;
 
-public class IMat {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.ResourceBundle;
+
+public class IMat extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("imat/resources/IMat");
+
+        Parent root = FXMLLoader.load(getClass().getResource("imat_main.fxml"), bundle);
+
+        Scene scene = new Scene(root, 1440, 1024);
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
 }
