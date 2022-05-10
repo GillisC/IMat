38d34d2bf236046ef82ab5335bb0127c8942c56f
Annheij <ann.heijkenskjold@gmail.com>
2022-05-10 11:40:57 +0200

    Börjat på detaljvyn
    FXML-saker fixade i controller och i scenebuilderr

--- src/imat/ImatDetaljvyController.java
+++ src/imat/ImatDetaljvyController.java
@@ -1,4 +1,33 @@
 package imat;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.awt.*;
+
 public class ImatDetaljvyController {
+
+    private Product product;
+
+
+    @FXML private AnchorPane detaljvyWindow;
+    @FXML private ImageView productImage;
+    @FXML private Label productName;
+    @FXML private Label productCategory;
+    @FXML private Label productWeight;
+    @FXML private Label productDetail;
+    @FXML private Label productHome;
+    @FXML private Label breadcrumbArrow;
+    @FXML private ImageView addToCartImage;
+    @FXML private ImageView closeImage;
+
+    public productItem(Product product) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("imat_detaljvy.fxml"));
+
+    }
 }
+
