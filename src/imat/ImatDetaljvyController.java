68a064b2c993ad7e1e3923cfd27ed0e38962677a
Annheij <ann.heijkenskjold@gmail.com>
2022-05-18 16:50:42 +0200

    detaljvyskit

--- src/imat/ImatDetaljvyController.java
+++ src/imat/ImatDetaljvyController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.Product;
@@ -55,12 +56,12 @@ public class ImatDetaljvyController extends AnchorPane{
         this.product = product;
         this.dataModel = dataModel;
         this.imat = imat;
+        Image image = dataModel.getFXImage(product);
 
-        productImage.setImage(productImage.getImage());
+        productImage.setImage(image);
         productName.setText(product.getName());
         productCategory.setText(String.valueOf(product.getCategory()));
         productWeight.setText(String.valueOf(product.getPrice()) + "/" + product.getUnit());
-
  }
      @FXML
      protected void clickedAddToCart(){
