8c43cf8f1be849a52238c2bdb6ac3982eb67d1e3
GillisC <timcarlsson0203@gmail.com>
2022-05-22 00:52:56 +0200

    nästan klar

--- src/imat/iMatComplete.java
+++ src/imat/iMatComplete.java
@@ -3,9 +3,12 @@ package imat;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.shape.Ellipse;
+import se.chalmers.cse.dat216.project.CreditCard;
+import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.Product;
 import se.chalmers.cse.dat216.project.ShoppingItem;
 
@@ -16,13 +19,27 @@ public class iMatComplete extends wizard implements ShoppingCartManager {
 
     @FXML private AnchorPane completeRootAnchorPane;
     @FXML private FlowPane completeFlowPane;
-    @FXML private Button complete,back4,shoppingCart;
-    @FXML private Ellipse selectDateTime, pay, delivery;
+    @FXML private Button complete,back4, changeDelivery, changePay;
+    @FXML private Label fullNameLabel, emailLabel, addressPostNumLabel, phoneNumLabel, cardNumLabel, expirationLabel, securityCode;
 
     IMatDataModel iMatDataModel = IMatDataModel.getInstance();
+    Customer customer;
+    CreditCard creditCard;
 
     public void initialize() {
+        customer = iMatDataModel.getCustomer();
+        creditCard = iMatDataModel.getCreditCard();
+
         updateFlowPane();
+
+        fullNameLabel.setText(customer.getFirstName() + " " + customer.getLastName());
+        emailLabel.setText(customer.getEmail());
+        addressPostNumLabel.setText(customer.getAddress() + ", " + customer.getPostCode());
+        phoneNumLabel.setText(customer.getMobilePhoneNumber());
+
+        cardNumLabel.setText(creditCard.getCardNumber());
+        expirationLabel.setText(creditCard.getValidMonth() + "/" + creditCard.getValidYear());
+        securityCode.setText(firstDigit(creditCard.getVerificationCode()) + "xx");
     }
 
 
@@ -43,7 +60,9 @@ public class iMatComplete extends wizard implements ShoppingCartManager {
         iMatDataModel.removeFromShoppingCart(product);
         updateFlowPane();
     }
+
     public void CompleteButtonPressed() {
+        iMatDataModel.placeOrder(true);
         navigateTo("iMatEnd.fxml", completeRootAnchorPane);
     }
 
@@ -67,49 +86,8 @@ public class iMatComplete extends wizard implements ShoppingCartManager {
         shoppingCartPressed (completeRootAnchorPane);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*   public void CompleteButtonPressed() {
-        try {
-            AnchorPane root = FXMLLoader.load(getClass().getResource("iMatEnd.fxml"));
-            completeRootAnchorPane.getChildren().setAll(root);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static int firstDigit(int n) {
+        while (n < -9 || 9 < n) n /= 10;
+        return Math.abs(n);
     }
-
-    public void back4ButtonPressed() {
-        try {
-            AnchorPane root = FXMLLoader.load(getClass().getResource("iMatDelivery.fxml"));
-            completeRootAnchorPane.getChildren().setAll(root);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-  */
 }
